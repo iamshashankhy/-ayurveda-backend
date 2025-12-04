e47ee19fed0ea836a4e02e0a4012afaa379bd9ec
Shashank H Y <shashankyadukumar@gmail.com>
2025-12-04 21:57:43 +0530

    Add API test endpoint

--- health/urls.py
+++ health/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.http import JsonResponse
 from .views import (
     AnalyzeDoshaView,
     PredictCancerView,
@@ -8,7 +9,21 @@ from .views import (
     ReviewsView,
 )
 
+def api_test(request):
+    return JsonResponse({
+        'status': 'API is working',
+        'endpoints': [
+            '/api/analyze_dosha/',
+            '/api/predict_cancer/',
+            '/api/assessment/combined/',
+            '/api/assessment/questions/',
+            '/api/profile/',
+            '/api/reviews/',
+        ]
+    })
+
 urlpatterns = [
+    path('', api_test, name='api_test'),  # Test endpoint
     path('analyze_dosha/', AnalyzeDoshaView.as_view(), name='analyze_dosha'),
     path('predict_cancer/', PredictCancerView.as_view(), name='predict_cancer'),
     path('assessment/combined/', CombinedAssessmentView.as_view(), name='combined_assessment'),
@@ -16,5 +31,3 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('reviews/', ReviewsView.as_view(), name='reviews'),
 ]
-
-
